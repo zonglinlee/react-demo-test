5ea378c06c771d6af76cb83f6dede6faedf2390d
zonglinlee <173389705@qq.com>
2023-03-20 09:58:02 +0800

    why useTimeout hooks works

--- src/demo/hooks/useTimeout.tsx
+++ src/demo/hooks/useTimeout.tsx
@@ -3,11 +3,13 @@ import {useEffect, useRef} from "react";
 export default function useTimeout(callback, delay) {
     const memorizeCallback = useRef()
     useEffect(() => {
+        console.log("set useTimeout callback") // 这里每次都会调用(因为每次传入的 callback 都不是同一个函数，是一个新的函数)
         memorizeCallback.current = callback
     }, [callback])
     useEffect(() => {
         if (delay !== null) {
             const timer = setTimeout(() => {
+                console.log("useTimeout callback called") // delay 不变的情况下， 这里只会调用 一次
                 // @ts-ignore
                 memorizeCallback.current()
             }, delay)
--- src/demo/setTimoutProblem/NoneSetTimeoutProblem.tsx
+++ src/demo/setTimoutProblem/NoneSetTimeoutProblem.tsx
@@ -4,12 +4,12 @@ import useTimeout from "../hooks/useTimeout";
 const SetTimeoutProblem: React.FC = (props: any) => {
     const [counter1, setCounter1] = useState(1)
     const [counter2, setCounter2] = useState(1)
-    useEffect(() => {
-        setCounter1(88)
-    }, [])
     useTimeout(() => {
         setCounter2(counter1)
     }, 3000)
+    useEffect(() => {
+        setCounter1(88)
+    }, [])
     return (
         <div>useTimeout 解决 hooks 的闭包缺陷 ---&gt; [ counter1: {counter1} | counter2 : {counter2} ]</div>
     )
